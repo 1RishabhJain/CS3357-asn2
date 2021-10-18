63a9049e8d849e339d0b28ac7229685ff16e72a0
Rishabh Jain <45084203+1RishabhJain@users.noreply.github.com>
2021-10-18 19:13:28 -0400

    Implemented selectors

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ import argparse
 from urllib.parse import urlparse
 import signal
 
-
+'''
 def signalHandler(sig, frame):
     print("Interrupt received, shutting down ...")
     discMsg = "DISCONNECT " + clientUsername + " CHAT/1.0"
@@ -23,78 +23,87 @@ def checkDisconnect():
         return True
     else:
         return False
+'''
 
 
-if len(sys.argv) == 3:
-    # Using argparse
-    parser = argparse.ArgumentParser()
-    # The first argument: the user name for the chat client to use for its user
-    parser.add_argument("username")
-    # The second argument: address for the chat server to connect to
-    parser.add_argument("address")
-    # Parsing the arguments
-    args = parser.parse_args()
+def validateArgs():
+    if len(sys.argv) == 3:
+        # Using argparse
+        parser = argparse.ArgumentParser()
+        # The first argument: the user name for the chat client to use for its user
+        parser.add_argument("username")
+        # The second argument: address for the chat server to connect to
+        parser.add_argument("address")
+        # Parsing the arguments
+        args = parser.parse_args()
 
-    # Storing parsed values into variables
-    clientUsername = args.username
-    url = urlparse(args.address)
-    hostname = url.hostname
-    port = url.port
+        # Storing parsed values into variables
+        username = args.username
+        url = urlparse(args.address)
+        argHostname = url.hostname
+        argPort = url.port
 
-    # Checking validity of provided arguments
-    if clientUsername.isalpha():
-        if bool(url.scheme) and bool(url.port):
-            pass
+        # Checking validity of provided arguments
+        if username.isalpha():
+            if bool(url.scheme) and bool(url.port):
+                return username, argHostname, argPort
+            else:
+                print("Error with address, please try again")
+                exit()
         else:
-            print("Error with address, please try again")
+            print("Error with username, please try again")
             exit()
     else:
-        print("Error with username, please try again")
+        print("Error with arguments, please try again")
         exit()
 
-    # Remainder of program executes if the username, host, and port are valid
-    print("Connecting to server ...")
 
-    # Creating socket and connecting
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    # Checking for connection errors
-    try:
-        clientSocket.connect((hostname, port))
-        print("Connection to server established. Sending intro message ...\n")
-    except ConnectionError or ConnectionRefusedError:
-        print("Connection unsuccessful, check address details and try again")
-        exit()
+# Calls function to validate the arguments, if there is any error then it will exit the program
+clientUsername, hostname, port = validateArgs()
 
-    # Creating and sending registration message
-    regMsg = "REGISTER " + clientUsername + " CHAT/1.0"
-    clientSocket.send(regMsg.encode())
+# Remainder of program executes if the username, host, and port are valid
+print("Connecting to server ...")
 
-    # Receiving registration response from server
-    regMsgResponse = clientSocket.recv(1024).decode()
-
-    # Error with registration
-    if regMsgResponse == "400 Invalid registration" or regMsgResponse == "401 Client already registered":
-        print(regMsgResponse)
-        exit()
-
-    # Successful registration
-    if regMsgResponse == "200 Registration successful":
-        print("Registration successful. Ready for messaging!")
-
-    # Sends client socket information to server
-    clientSocket.send(str(clientSocket.getsockname()).encode())
-
-    while True:
-        signal.signal(signal.SIGINT, signalHandler)
-        #checkDisconnect()
-        inputMessage = input('Input: ')
-        message = ("@" + clientUsername + " " + inputMessage)
-        clientSocket.send(message.encode())
-        receivedMessage = clientSocket.recv(1024)
-        print('From Server:', receivedMessage.decode())
-    signal.pause()
-    clientSocket.close()
-
-else:
-    print("Error with arguments, please try again")
+# Creating socket and connecting
+clientSocket = socket(AF_INET, SOCK_STREAM)
+# Checking for connection errors
+try:
+    clientSocket.connect((hostname, port))
+    print("Connection to server established. Sending intro message ...\n")
+except ConnectionError or ConnectionRefusedError:
+    print("Connection unsuccessful, check address details and try again")
     exit()
+
+# Creating and sending registration message
+regMsg = "REGISTER " + clientUsername + " CHAT/1.0"
+clientSocket.send(regMsg.encode())
+
+# Receiving registration response from server
+regMsgResponse = clientSocket.recv(1024).decode()
+
+# Error with registration
+if regMsgResponse == "400 Invalid registration" or regMsgResponse == "401 Client already registered":
+    print(regMsgResponse)
+    exit()
+
+# Successful registration
+if regMsgResponse == "200 Registration successful":
+    print("Registration successful. Ready for messaging!")
+
+# Sends client socket information to server
+clientSocket.send(str(clientSocket.getsockname()).encode())
+
+while True:
+    # signal.signal(signal.SIGINT, signalHandler)
+    # checkDisconnect()
+    inputMessage = input('Input: ')
+    message = ("@" + clientUsername + " " + inputMessage)
+
+    clientSocket.send(message.encode())
+
+    receivedMessage = clientSocket.recv(1024)
+
+    print('From Server:', receivedMessage.decode())
+
+signal.pause()
+clientSocket.close()
--- server.py
+++ server.py
@@ -6,91 +6,130 @@ Rishabh Jain
 from random import *
 from socket import *
 import signal
+import select
 
-
-def signalHandler(sig, frame):
+'''
+def signalHandler(sig, frame, receiveSocket):
     print("Interrupt received, shutting down ...")
     discMsg = "DISCONNECT CHAT/1.0"
-    connectionSocket.send(discMsg.encode())
+    receiveSocket.send(discMsg.encode())
     exit()
+'''
 
 
-def checkRegistration(checkMessage):
+def checkRegistration(checkMessage, receiveSocket):
     # The split command is put into a try and exceptions are caught
     try:
         (reg, client, chat) = checkMessage.split(" ")
     except ValueError:
         # Print and send 400 Invalid registration
         print("400 Invalid registration")
-        connectionSocket.send("400 Invalid registration".encode())
+        receiveSocket.send("400 Invalid registration".encode())
         exit()
 
     validMessage = "REGISTER " + client + " CHAT/1.0"
     # Check format of registration message and presence of username in list
     if checkMessage == validMessage:
         # Checks if client is already registered
-        if client not in clientList:
-            clientList.append(client)
-            connectionSocket.send("200 Registration successful".encode())
-            print("Accepted connection from client address: " + connectionSocket.recv(1024).decode())
+        if client not in clientList.values():
+            socketList.append(receiveSocket)
+            clientList[receiveSocket] = client
+            receiveSocket.send("200 Registration successful".encode())
+            print("Accepted connection from client address: " + receiveSocket.recv(1024).decode())
             print("Connection to client established, waiting to receive messages from user '" + client + "'")
         # Client already exists, send error message
         else:
             print("401 Client already registered")
-            connectionSocket.send("401 Client already registered".encode())
+            receiveSocket.send("401 Client already registered".encode())
             exit()
 
     # Error with message format
     else:
         print("400 Invalid registration")
-        connectionSocket.send("400 Invalid registration".encode())
+        receiveSocket.send("400 Invalid registration".encode())
         exit()
 
     return client
 
 
+def receiveMessage(receiveSocket, boolean):
+    try:
+        message = receiveSocket.recv(1024).decode()
+        if boolean:
+            checkRegistration(message, receiveSocket)
+        else:
+            print("Received message from user " + clientList[receiveSocket] + ": " + message)
+            return message
+    except:
+        print("Error reading message")
+        pass
+
+
+'''
 def checkDisconnect(message, user):
     # Check format of disconnect message
     if message == "DISCONNECT " + user + " CHAT/1.0":
         return True
     else:
         return False
-
-
-#socketList = []
-clientList = []
+'''
 
 # Server Port, and Address
 serverPort = randrange(5000, 12000)
 
 # Server socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
+# Server will listen for connections
+serverSocket.listen()
+socketList = [serverSocket]
+clientList = {}
 
 # Print statements indicating that the server is waiting for clients on a particular port
 print('Will wait for client connections at port', serverPort)
 print('Waiting for incoming client connections ...')
 
-connectionSocket, addr = serverSocket.accept()
+# clientSocket, addr = serverSocket.accept()
 
 # Receives registration message from client
-regMsg = connectionSocket.recv(1024).decode()
-clientUsername = checkRegistration(regMsg)
+# regMsg = clientSocket.recv(1024).decode()
+# clientUsername = checkRegistration(regMsg)
 
 while True:
-    signal.signal(signal.SIGINT, signalHandler)
-    message = connectionSocket.recv(1024).decode()
-    print("Received message from user " + clientUsername + ": " + message)
-    if checkDisconnect(message, clientUsername):
+    # Checks for ^C
+
+    # signal.signal(signal.SIGINT, signalHandler)
+
+    readSockets, _, exceptionSockets = select.select(socketList, [], socketList)
+
+
+    for notifiedSocket in readSockets:
+
+        if notifiedSocket == serverSocket:
+
+            clientSocket, clientAddress = serverSocket.accept()
+
+            # recieve message function is called and passes boolean true that signifies it's a new connection
+            clientMessage = receiveMessage(clientSocket, True)
+
+        else:
+
+            clientMessage = receiveMessage(notifiedSocket, False)
+            for clientSocket in clientList:
+                if clientSocket != notifiedSocket:
+                    clientSocket.send(clientMessage.encode())
+
+    '''
+    if checkDisconnect(clientMessage, clientUsername):
         print("Disconnecting user " + clientUsername)
         clientList.remove(clientUsername)
         if len(clientList) == 0:
             print("No more clients, good bye")
             signal.pause()
-            connectionSocket.close()
+            clientSocket.close()
             exit()
     else:
-        connectionSocket.send(message.encode())
-    print(clientList)
-
+        clientSocket.send(clientMessage.encode())
+    '''
+    # print(clientList)
